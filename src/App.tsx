1191cf9140b30e965c254082e50bf536ad177ccf
Joy Brar <joybrar2001@gmail.com>
2025-10-20 10:08:17 +0530

    Added Features Section and Macbook 3D Model

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import ProductViewer from "./components/ProductViewer";
 import Showcase from "./components/Showcase";
 import Performance from "./components/Performance";
+import Features from "./components/Features";
 
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
@@ -17,6 +18,7 @@ const App = () => {
       <ProductViewer />
       <Showcase />
       <Performance />
+      <Features />
     </main>
   );
 }
